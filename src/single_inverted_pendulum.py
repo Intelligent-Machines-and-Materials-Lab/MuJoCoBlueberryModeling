32b7ec13f63e569c0f8fee517cc2bb27676c2172
Hannah Kolano <hkolano@comcast.net>
2025-06-19 15:15:08 -0700

    initial commit

---
+++ src/single_inverted_pendulum.py
@@ -0,0 +1,31 @@
+import distutils.util
+import os
+
+try:
+  print('Checking that the installation succeeded:')
+  import mujoco
+  mujoco.MjModel.from_xml_string('<mujoco/>')
+except Exception as e:
+  raise e from RuntimeError(
+      'Something went wrong during installation. Check the shell output above '
+      'for more information.\n'
+      'If using a hosted Colab runtime, make sure you enable GPU acceleration '
+      'by going to the Runtime menu and selecting "Choose runtime type".')
+
+print('Installation successful.')
+
+# Other imports and helper functions
+import time
+import itertools
+import numpy as np
+
+# Graphics and plotting.
+import mediapy as media
+import matplotlib.pyplot as plt
+
+# More legible printing from numpy.
+np.set_printoptions(precision=3, suppress=True, linewidth=100)
+
+from IPython.display import clear_output
+clear_output()
+
