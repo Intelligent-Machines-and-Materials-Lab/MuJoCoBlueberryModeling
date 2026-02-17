b983216d770c290f9f44834e03450766843314bf
hkolano <hkolano@comcast.net>
2026-02-16 19:13:50 -0800

    cropping out real data until compression starts

--- src/preprocess_push_data.py
+++ src/preprocess_push_data.py
@@ -6,11 +6,16 @@ def preprocess_push_data(input_file, output_file):
     # Read the input CSV file
     data = pd.read_csv(input_file)
 
+    # find the first load value that is positive and get the index of that row
+    first_positive_load_index = data[data['Load Cell Reading (gf)'] > 0].index[0]
+    print("First positive load index: ", first_positive_load_index)
+
+    # drop all rows before the first positive load value
+    data = data.iloc[first_positive_load_index:]
+    print("Data after dropping rows before first positive load value: ", data.head())
+
     # create a new column for force called Load (N) and convert from gF to N
     data['Load (N)'] = data['Load Cell Reading (gf)'] * 0.00980665
-
-    # zero the Load (N) column by subtracting the initial value from all values
-    data['Load (N)'] = data['Load (N)'] - data['Load (N)'].iloc[0]
 
     # zero the Displacement (mm) column by subtracting the initial value from all values
     data['Displacement (mm)'] = data['Displacement (mm)'] - data['Displacement (mm)'].iloc[0]    
@@ -22,9 +27,10 @@ def preprocess_push_data(input_file, output_file):
 if __name__ == "__main__":
     # iterate through all files in data directory
     print(os.listdir())
+    # preprocess_push_data('data/push_data/bush_1_branch_1_trial_2.csv')
     for filename in os.listdir('data/push_data'):
         print(filename)
         input_file = os.path.join('data/push_data', filename)
-        output_file = os.path.join('data/zeroed_push_data', filename)
+        output_file = os.path.join('data/cropped_push_data', filename)
 
         preprocess_push_data(input_file, output_file)
